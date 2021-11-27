4af73c9b2ccf9b6d14a121f58ed45ce396e0e7c3
kbgro <daniendegwa@gmail.com>
2021-11-27 19:30:09 +0300

    add export support for __all__

--- pyroxy/__init__.py
+++ pyroxy/__init__.py
@@ -1,5 +1,9 @@
 """Top-level package for pyroxy."""
+from .address import Anonymity, Protocol
+from .pyroxy import filter_proxy_list, proxy_list
 
 __author__ = """Daniel Ndegwa"""
 __email__ = "daniendegwa@gmail.com"
 __version__ = "0.1.0"
+
+__all__ = ["proxy_list", "filter_proxy_list", "Protocol", "Anonymity"]
